168f1c30440cee49023dc7efdaee905c3d371e78
abrarfaiazatuno <abrarfaiaz0@gmail.com>
2023-01-29 23:05:14 +0600

    trying server side rendering

---
+++ server.js
@@ -0,0 +1,21 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  console.log("request made");
+
+  res.setHeader("content-type", "text/html");
+
+  fs.readFile("./views/index.html", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.end(data);
+    }
+  });
+});
+
+server.listen(3000, "localhost", () => {
+  console.log("listening for requests on port 3000");
+});
